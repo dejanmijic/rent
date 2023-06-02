49f9d25b5b3ccd357ce2c610485eaa2486785659
dejanmijic <dejan8e@gmail.com>
2023-06-02 16:40:00 +0200

    Registration form

--- rent-a-car-fe/src/views/Register.vue
+++ rent-a-car-fe/src/views/Register.vue
@@ -1,13 +1,117 @@
 <template>
-  <div class="main">
-    <h1>Register page</h1>
+  <div class="register-form">
+    <h1>Registration</h1>
+    <form v-on:submit.prevent="handleSubmit">
+      <div class="form-item">
+        <label>Username:</label>
+        <input type="text" v-model="username" />
+      </div>
+      <div class="form-item">
+        <label>Password:</label>
+        <input type="password" v-model="password" />
+      </div>
+      <div class="form-item">
+        <label>Repeat password:</label>
+        <input type="password" v-model="repeatedPassword" />
+      </div>
+      <div class="form-item">
+        <label>First name:</label>
+        <input type="text" v-model="firstName" />
+      </div>
+      <div class="form-item">
+        <label>Last name:</label>
+        <input type="text" v-model="lastName" />
+      </div>
+      <div class="form-item">
+        <label>Gender:</label>
+        <div class="gender">
+          <div>
+            <input type="radio" name="gender" value="male" v-model="gender" />
+            <label>Male</label>
+          </div>
+          <div>
+            <input type="radio" name="gender" value="female" v-model="gender" />
+            <label>Female</label>
+          </div>
+        </div>
+      </div>
+      <div class="form-item">
+        <label>Date of birth:</label>
+        <input type="date" v-model="dateOfBirth" />
+      </div>
+      <div>
+        <button class="submit" type="submit">Submit</button>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
   name: "Register-Page",
+
+  data() {
+    return {
+      username: "",
+      password: "",
+      repeatedPassword: "",
+      firstName: "",
+      lastName: "",
+      gender: "male",
+      dateOfBirth: "",
+    };
+  },
+  methods: {
+    handleSubmit() {
+      if (this.password !== this.repeatedPassword) {
+        alert("Passwords do not match");
+        return;
+      }
+      alert("Success!");
+    },
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.register-form {
+  width: 50%;
+  margin: auto;
+  text-align: center;
+}
+
+.form-item {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  width: 80%;
+  margin: auto;
+  padding: 12px;
+}
+
+.form-item label {
+  text-align: left;
+  font-weight: 600;
+  margin-bottom: 6px;
+}
+
+.gender {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.submit {
+  margin: 24px;
+  padding: 16px;
+  border-radius: 6px;
+  width: 50%;
+  outline: none;
+  cursor: pointer;
+  border: none;
+  font-family: Avenir;
+  font-size: 24px;
+  font-weight: 600;
+  background-color: rgb(160, 226, 160);
+}
+</style>
